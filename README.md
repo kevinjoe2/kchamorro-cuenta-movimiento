319e10171adcf29f5f6499b33f31e473f7483bf2
Joel Chamorro <kchamorro@ec.krugercorp.com>
2023-11-20 18:09:11 -0500

    KCH - First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Java 17
+Gradle 7
+Spring 3.1.5
+Postgresql
---
+++ src/test/java/com/neoris/cuentamovimiento/CuentaMovimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoris.cuentamovimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaMovimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
